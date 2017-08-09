96185fd82328d5c1aafc53b70c1182fe7fea8aa8
Chris Gaona <thebeloved88@sbcglobal.net>
2017-08-09 09:05:31 -0700

    Wires up new fact on click of button

--- app/src/main/java/com/chrisgaona/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/chrisgaona/funfacts/FunFactsActivity.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class FunFactsActivity extends AppCompatActivity {
     // Declare our view variables
     private TextView mFactTextView;
@@ -24,7 +26,25 @@ public class FunFactsActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 // the button was clicked so update the fact TextView with a new fact
-                String fact = "Ostriches can run faster than horses.";
+                String[] facts = {
+                    "Ants stretch when they wake up in the morning.",
+                    "Ostriches can run faster than horses.",
+                    "Olympic gold medals are actually made mostly of silver.",
+                    "You are born with 300 bones; by the time you are an adult you will have 206.",
+                    "It takes about 8 minutes for light from the Sun to reach Earth.",
+                    "Some bamboo plants can grow almost a meter in just one day.",
+                    "The state of Florida is bigger than England.",
+                    "Some penguins can leap 2-3 meters out of the water.",
+                    "On average, it takes 66 days to form a new habit.",
+                    "Mammoths still walked the earth when the Great Pyramid was being built."
+                };
+                // randomly select a fact
+                String fact = "";
+                Random randomGenerator = new Random();
+                int randomNumber = randomGenerator.nextInt(facts.length);
+                fact = facts[randomNumber];
+
+                // update the screen with our dynamic fact
                 mFactTextView.setText(fact);
             }
         };
